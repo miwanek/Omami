6c4d4e39b3c464fc9aed17168ef2b6a229a3fcce
Michal <m.iwanek@stud.elka.pw.edu.pl>
2019-03-23 12:35:59 +0100

    Init commit

---
+++ src/main/java/olaf/cafe/omami/OmamiApplication.java
@@ -0,0 +1,13 @@
+package olaf.cafe.omami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmamiApplication.class, args);
+	}
+
+}
