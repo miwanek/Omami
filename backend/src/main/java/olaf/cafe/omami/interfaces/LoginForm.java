e1cf6f19290177f6931db99c8c7672defcef066a
Michal <m.iwanek@stud.elka.pw.edu.pl>
2019-05-01 21:50:10 +0200

    #9 Refactored login endpoint

---
+++ backend/src/main/java/olaf/cafe/omami/interfaces/LoginForm.java
@@ -0,0 +1,11 @@
+package olaf.cafe.omami.interfaces;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginForm {
+    private String username;
+    private String password;
+}
--- backend/src/main/java/olaf/cafe/omami/interfaces/UserController.java
+++ backend/src/main/java/olaf/cafe/omami/interfaces/UserController.java
@@ -5,23 +5,23 @@ import olaf.cafe.omami.domain.User;
 import olaf.cafe.omami.domain.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@Service
+@RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping
-    public ResponseEntity<Void> checkIfLoginSuccessful(@RequestParam String username, @RequestParam String password) {
-        Optional<User> user = userRepository.findByLoginAndPassword(username, password);
+    @GetMapping(path = "/login")
+    public ResponseEntity<Void> checkIfLoginSuccessful(@RequestBody LoginForm loginForm) {
+        Optional<User> user = userRepository.findByLoginAndPassword(loginForm.getUsername(), loginForm.getPassword());
 
         if (user.isPresent()){
             return new ResponseEntity<>(HttpStatus.OK);
