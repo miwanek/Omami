604ae04d6e0267534cb63a9a364cf21139af2aca
AnnaYelkina <anna.yelkina@savangard.com>
2019-05-17 13:37:13 +0200

    praca nad rejestracja

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import Form from 'react-bootstrap/Form';
-import '../style.css'
+import '../login.css'
 
 export default class Login extends Component {
     constructor() {
@@ -61,37 +61,28 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div className="Login">
-                {/*<label className="greeting">hi, dear</label>*/}
-
-                <Form onSubmit={this.handleSubmit} >
-                    <Form.Label className="label">username: </Form.Label>
-                    <Form.Group controlId="email" bsSize="large">
-                        <Form.Control className="input"
-                                      autoFocus
-                                      //type="email"
-                                      value={this.state.email}
-                                      onChange={this.handleChange}
-                        />
-                    </Form.Group>
-                    <Form.Label className="label">password: </Form.Label>
-                    <Form.Group controlId="password" bsSize="large">
-                        <Form.Control className="input"
-                                      value={this.state.password}
-                                      onChange={this.handleChange}
-                                      type="password"
-                        />
-                    </Form.Group>
-
-                    <button type = "submit" block class="login-btn"
-                            disabled={!this.validateForm()}
-                             onClick = {this.authorize}//.bind(this)}
-                    >
-                        Login
-                    </button>
-                </Form>
+            <body className="align">
+            <div className="grid align__item">
+            <div className="register">
+            <svg xmlns="http://www.w3.org/2000/svg" class="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#8ceabb"/><stop offset="100%" stop-color="#378f7b"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
+            <h2>Sign Up</h2>
+            <form action="" method="post" class="form">
+            <div class="form__field">
+            <input type="email" placeholder="info@mailaddress.com">
+            </input>
+            <div class="form__field">
+          <input type="password" placeholder="••••••••••••">
+          </input>
+          <div class="form__field">
+          <input type="submit" value="Sign Up"></input>
+        </div>
+        </div>
             </div>
-
+          
+            </form>
+            <p>Already have an accout? <a href="#">Log in</a></p>
+            </div></div>
+            </body>
         );
     }
 }
