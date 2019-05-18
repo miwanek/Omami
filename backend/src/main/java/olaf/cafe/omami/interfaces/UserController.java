273625a5d96d835a972754a48a4d1658dcd449c7
Michal <m.iwanek@stud.elka.pw.edu.pl>
2019-05-18 19:16:35 +0200

    Added endpoint for fetching all Closes #18

--- backend/src/main/java/olaf/cafe/omami/interfaces/UserController.java
+++ backend/src/main/java/olaf/cafe/omami/interfaces/UserController.java
@@ -6,12 +6,16 @@ import olaf.cafe.omami.domain.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +34,14 @@ public class UserController {
         } else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDTO(user.getId(), user.getLogin()))
+                .collect(Collectors.toList());
     }
 }
---
+++ backend/src/main/java/olaf/cafe/omami/interfaces/UserDTO.java
@@ -0,0 +1,11 @@
+package olaf.cafe.omami.interfaces;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDTO {
+    private Integer id;
+    private String username;
+}
