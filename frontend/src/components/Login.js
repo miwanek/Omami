77a73eeae89385ef6e94c23deb4c6e0781a343ce
ayelkina <yelkina.anna@gmail.com>
2019-05-23 12:37:45 +0200

    zmiana okna logowania, dodanie rejestracji

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -20,7 +20,7 @@ export default class Login extends Component {
     }
 
     handleChange = event => {
-       
+
         this.setState({
             [event.target.id]: event.target.value
         });
@@ -30,8 +30,8 @@ export default class Login extends Component {
     handleSubmit = event => {
         event.preventDefault();
     }
-    
-     async authorize () {
+
+    async authorize() {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
         const data = this.createJsonLoginForm(this.state.email, this.state.password);
@@ -43,45 +43,58 @@ export default class Login extends Component {
         const request = new Request('https://omami.herokuapp.com/users/login', options);
         const response = await fetch(request);
         const status = await response.status;
-         //this.props.goToChat(true);
-        if(status === 200) {
+        //this.props.goToChat(true);
+        if (status === 200) {
             this.props.goToChat(true);
             //this.state.email = "JEST";
-        }
-
-        else if(status === 404) {
+        } else if (status === 404) {
             this.state.email = "Username or password incorrect!";
         }
     }
 
-    createJsonLoginForm(email, password){
-        var json = '{"username": "' + email + '", "password": "'+ password + '"}';
+    createJsonLoginForm(email, password) {
+        var json = '{"username": "' + email + '", "password": "' + password + '"}';
         return json;
     }
 
     render() {
         return (
             <body className="align">
-            <div className="grid align__item">
-            <div className="register">
-            <svg xmlns="http://www.w3.org/2000/svg" class="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#8ceabb"/><stop offset="100%" stop-color="#378f7b"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
-            <h2>Sign Up</h2>
-            <form action="" method="post" class="form">
-            <div class="form__field">
-            <input type="email" placeholder="info@mailaddress.com">
-            </input>
-            <div class="form__field">
-          <input type="password" placeholder="••••••••••••">
-          </input>
-          <div class="form__field">
-          <input type="submit" value="Sign Up"></input>
-        </div>
-        </div>
+            <div className="grid">
+                <Form onSubmit={this.handleSubmit}>
+                    <div className="register">
+                        <img src="cloud-plain.ico"/>
+                        <h2>Hello!</h2>
+                        <Form.Group controlId="email" bsSize="large">
+                        <Form.Control className="input"
+                                      placeholder="info@mailaddress.com"
+                                      autoFocus
+                                      value={this.state.email}
+                                      onChange={this.handleChange}
+                        />
+                        </Form.Group>
+                        <Form.Group controlId="password" bsSize="large">
+                        <Form.Control className="input"
+                                      placeholder="•••••••••••••"
+                                      value={this.state.password}
+                                      onChange={this.handleChange}
+                                      type="password"
+                        />
+                        </Form.Group>
+                            <button type="submit" block className="login-btn"
+                                    disabled={!this.validateForm()}
+                                    onClick={this.authorize}//.bind(this)}
+                            > Login
+                            </button>
+                            <button type="submit" block className="signup-btn"
+                                    // disabled={!this.validateForm()}
+                                    // onClick={this.authorize}//.bind(this)}
+                            > Register
+                            </button>
+
+                    </div>
+                </Form>
             </div>
-          
-            </form>
-            <p>Already have an accout? <a href="#">Log in</a></p>
-            </div></div>
             </body>
         );
     }
